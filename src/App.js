37ce62a2fa716c63057f58caf62b99cd2183448f
Alex <alexgjohn@outlook.com>
2023-05-16 16:17:57 +0100

    mvp done, working on extensions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Buy shopping", highPriority: true },
+    { id: 2, name: "Clean bathroom", highPriority: false },
+    { id: 3, name: "Car's MOT", highPriority: false },
+  ])
+
+  const [newTask, setNewTask] = useState("")
+
+  const completeTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
+  const listTasks = tasks.map((task) => {
+    return(
+      <li key={task.id}>
+        {task.name}
+        <button onClick={() => completeTask(task.id)}>Task Complete?</button>
+      </li>
+    )
+  })
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault()
+    if (newTask !== "") {
+      const newTaskObj = { id: Date.now(), name: newTask, highPriority: false}
+      const nextTasks = [...tasks, newTaskObj]
+      setTasks(nextTasks)
+
+      setNewTask("")
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List - {tasks.length ? "No Rest For The Wicked!" : "Finally Done!"}</h1>
+      <ul>
+        {listTasks}
+      </ul>
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task">Add new task: </label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}/>
+        <label htmlFor="high-priority">High Priority?</label>
+        <input type="checkbox"></input>
+        <input type="submit" value="Save new task"/>
+      </form>
+
     </div>
   );
 }
